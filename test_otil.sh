15d23e0b6e81a1b872a4aae5761faf70ed3f4cdc
joao <joao@cramos.ch>
2025-10-23 00:24:48 +0200

    update wandb mode

--- test_otil.sh
+++ test_otil.sh
@@ -3,6 +3,6 @@ task=DFlex agent=DFlexAntOTIL task.env.env_name=ant \
 logdir="workdir/DFlexAnt10M-OTIL/$(date +%Y%m%d-%H%M%S.%2N)" \
 agent.otil.max_epochs=5000 agent.otil.max_agent_steps=10e6 \
 agent.network.actor_kwargs.mlp_kwargs.units=\[128,64,32\] \
-wandb.mode=disabled wandb.project=otil \
+wandb.mode=online wandb.project=otil \
 run=train_eval seed=100 \
 agent.otil.demos_path="experts/DFlex_ant_demos64_return9340_len1000.pt"
