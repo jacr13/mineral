1d9b4a6bcbd45f9ed9ca72db7c4f9815c2dfef88
Joao <joao@cramos.ch>
2025-10-30 15:37:58 +0100

    add path to docker_img

--- scripts/test_singularity.sh
+++ scripts/test_singularity.sh
@@ -3,7 +3,7 @@ git pull
 python spawner.py \
     --task_name sapo/dflex_ant.yaml \
     --docker \
-    --docker_image mineral.sif \
+    --docker_image /home/users/c/candidor/docker/mineral.sif \
     --deployment slurm  \
     --runtime 10m \
     --no-cleanup \
